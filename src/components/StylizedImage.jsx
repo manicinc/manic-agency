772a7bf4e50a6e925a57a042f809ac9c9450e927
Johnny Dunn <johnnyddunn@gmail.com>
2024-02-08 15:16:52 -0800

    edit services graphic

--- src/components/StylizedImage.jsx
+++ src/components/StylizedImage.jsx
@@ -1,6 +1,8 @@
+"use client";;
+import { useEffect } from "react";
+
 import clsx from "clsx";
 import { useId } from "react";
-import Image from "next/image";
 
 const shapes = [
   {
@@ -22,6 +24,14 @@ const shapes = [
 const StylizedImage = ({ shape = 0, className, ...props }) => {
   const id = useId();
   const { width, height, path } = shapes[shape];
+
+  let is_safari = false;
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+    is_safari = window.navigator.userAgent.toLowerCase().indexOf('safari/') > -1;
+  }, [is_safari]);
+
+
   return (
     <div
       className={clsx(
@@ -33,12 +43,35 @@ const StylizedImage = ({ shape = 0, className, ...props }) => {
         <g clipPath={`url(#${id}-clip)`} className="group">
           <g className="origin-center scale-100 transition duration-500 motion-safe:group-hover:scale-105">
             <foreignObject width={width} height={height}>
-              <Image
-                alt=""
-                className="w-full bg-neutral-100 object-cover"
-                style={{ aspectRatio: `${width} / ${height}` }}
-                {...props}
-              />
+                      {/* If isSafari */}
+              {is_safari && (
+                <svg className="Playground__svg ml-20" viewBox="0 0 100 120">
+                  <image x="0%" y="0%" width="50" height="100" preserveAspectRatio="xMidYMid slice" xlinkHref="https://images.unsplash.com/photo-1571977144562-3737f035296a?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ" id="my-image"></image>
+                  <div className="glitch-layers">
+                    <div className="glitch-layer"><img src="https://play-lh.googleusercontent.com/i1qvljmS0nE43vtDhNKeGYtNlujcFxq72WAsyD2htUHOac57Z9Oiew0FrpGKlEehOvo=w240-h480-rw" alt="Image"/></div>
+                    <div className="glitch-layer"><img src="https://play-lh.googleusercontent.com/i1qvljmS0nE43vtDhNKeGYtNlujcFxq72WAsyD2htUHOac57Z9Oiew0FrpGKlEehOvo=w240-h480-rw" alt="Image"/></div>
+                    <div className="glitch-layer"><img src="https://play-lh.googleusercontent.com/i1qvljmS0nE43vtDhNKeGYtNlujcFxq72WAsyD2htUHOac57Z9Oiew0FrpGKlEehOvo=w240-h480-rw" alt="Image"/></div>
+                  </div>
+              </svg>
+              )}
+              {/* If not isSafari */}
+              {!is_safari && (
+                <svg className="Playground__svg ml-20" viewBox="0 0 100 120">
+                  <defs>
+                    <filter id="filter" x="-20%" y="-20%" width="140%" height="140%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" colorInterpolationFilters="sRGB" filterRes="1">
+                      <feMorphology operator="dilate" radius="10 0" x="0%" y="0%" width="100%" height="100%" in="SourceGraphic" result="morphology1"></feMorphology>
+                    </filter>
+                    <filter id="filter-2" x="-20%" y="-20%" width="140%" height="140%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" colorInterpolationFilters="sRGB" filterRes="1">
+                      <feMorphology operator="dilate" radius="10 2" x="0%" y="0%" width="100%" height="100%" in="SourceGraphic" result="morphology1"></feMorphology>
+                    </filter>
+                    <filter id="filter-3" x="-20%" y="-20%" width="140%" height="140%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" colorInterpolationFilters="sRGB" filterRes="1">
+                      <feMorphology operator="dilate" radius="15 0" x="0%" y="0%" width="100%" height="100%" in="SourceGraphic" result="morphology1" filterRes="1"></feMorphology>
+                    </filter>
+                  </defs>
+                  <image x="0%" y="0%" height="120" preserveAspectRatio="xMidYMid slice" xlinkHref="https://images.unsplash.com/photo-1571977144562-3737f035296a?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ" id="my-image"></image>
+           
+              </svg>
+              )}
             </foreignObject>
           </g>
           <use
