027b1ab993fe396876b506fb534efd2ec75b7b5b
Johnny Dunn <johnnyddunn@gmail.com>
2024-02-08 22:35:44 -0800

    fixes

--- src/app/work/page.jsx
+++ src/app/work/page.jsx
@@ -87,8 +87,8 @@ const WorkPage = () => {
         <div id="item01" className="port">
           <a href="#item01" className="close text-slate-800"></a>
             <div className="description w-full">
-              <h1 className="text-slate-700 w-1/3 sm:w-3/3 text-2xl">Quire.work</h1>
-              <p className="text-slate-700 w-1/3 sm:w-3/3 text-sm overflow-y-auto">Quire is a platform where users can create their own autonomous AI agents to automate and perform any type of work. Quire agents can work together in a collaborative manner to achieve common goals. This was built in Next / React, TypeScript, LangChain, and LlamaIndex, and uses both open-source LLMs like Mistral and GPT-3.5 / GPT-4.</p>
+              <h1 className="text-slate-700 w-2/3 sm:w-1/3 text-2xl">Quire.work</h1>
+              <p className="text-slate-700 w-2/3 sm:w-1/3 text-sm overflow-y-auto">Quire is a platform where users can create their own autonomous AI agents to automate and perform any type of work. Quire agents can work together in a collaborative manner to achieve common goals. This was built in Next / React, TypeScript, LangChain, and LlamaIndex, and uses both open-source LLMs like Mistral and GPT-3.5 / GPT-4.</p>
               <br></br>
               <a className="w-full text-blue-400 hover:text-slate-400" href="https://quire.work" target="_blank">https://quire.work</a>
             </div>
@@ -97,9 +97,9 @@ const WorkPage = () => {
         <div id="item02" className="port">
         <a href="#item02"  className="close text-slate-800"></a>
           <div className="row">
-            <div className="description">
-              <h1 className="text-slate-700 w-1/3">Fortune.day</h1>
-              <p className="text-slate-700 w-1/3 text-sm overflow-y-auto">Fortune is a decentralized metaverse platform with an initial version that will exist on the web. Any type of app or game can exist in Fortune. Worlds are interconnected with persistent player profiles and inventories, with authentication being web3 wallet-based. Users can mint a Fortune NFT (ERC-1155) to gain access to the hosted metaverse and create their own game world where other players can join and interact with each other in an online manner through web sockets. Currency in the metaverse is tokenized with a ERC-20 token. This platform is currently still in-development with plans to be entirely open-source.</p>
+            <div className="description w-full">
+              <h1 className="text-slate-700 w-2/3 sm:w-1/3 ">Fortune.day</h1>
+              <p className="text-slate-700 w-2/3 sm:w-1/3 text-sm overflow-y-auto">Fortune is a decentralized metaverse platform with an initial version that will exist on the web. Any type of app or game can exist in Fortune. Worlds are interconnected with persistent player profiles and inventories, with authentication being web3 wallet-based. Users can mint a Fortune NFT (ERC-1155) to gain access to the hosted metaverse and create their own game world where other players can join and interact with each other in an online manner through web sockets. Currency in the metaverse is tokenized with a ERC-20 token. This platform is currently still in-development with plans to be entirely open-source.</p>
               <br></br>
               <a className="w-2/3 text-blue-400 hover:text-slate-400" href="https://fortune.day" target="_blank">https://fortune.day</a>
             </div>
@@ -109,9 +109,9 @@ const WorkPage = () => {
         <div id="item03" className="port">
         <a href="#item03" className="close text-slate-800"></a>
           <div className="row">
-            <div className="description">
-              <h1 className="text-slate-700 w-1/3">Hype.blog</h1>
-              <p className="text-slate-700 w-1/3 text-sm overflow-y-auto">Hype.blog is a digital media newsroom with AI journalists that can operate the newsroom entirely independently, by scouring content on the web to research topics to write on, and using generative AI to create visual and written content. Human staff can edit, create, and publish articles in a CMS to work together with the AI agents. This was built in React / Next, TypeScript, and Python, with a cognitive AI library we are developing that will be released open-source later in 2024.</p>
+            <div className="description w-full">
+              <h1 className="text-slate-700 w-2/3 sm:w-1/3">Hype.blog</h1>
+              <p className="text-slate-700 w-2/3 sm:w-1/3 text-sm overflow-y-auto">Hype.blog is a digital media newsroom with AI journalists that can operate the newsroom entirely independently, by scouring content on the web to research topics to write on, and using generative AI to create visual and written content. Human staff can edit, create, and publish articles in a CMS to work together with the AI agents. This was built in React / Next, TypeScript, and Python, with a cognitive AI library we are developing that will be released open-source later in 2024.</p>
               <br></br>
               <a className="w-2/3 text-blue-400 hover:text-slate-400" href="#" target="_blank">Link coming soon</a>
             </div>
