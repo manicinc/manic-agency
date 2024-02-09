50b46a3a4c2db462dff28dae274e11cab2ab9b35
Johnny Dunn <johnnyddunn@gmail.com>
2024-02-08 18:27:56 -0800

    fixes

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,8 +1,6 @@
 "use client";;
 import SectionIntro from "./SectionIntro";
 import Container from "./Container";
-import StylizedImage from "./StylizedImage";
-import imageLaptop from "../images/laptop.jpg";
 import List, { ListItem } from "./List";
 
 const Services = () => {
@@ -21,12 +19,26 @@ const Services = () => {
       <Container className="mt-16">
         <div className="lg:flex lg:items-center lg:justify-end">
           <div className="flex justify-center lg:w-1/2 lg:justify-end lg:pr-12">
-            <div className="w-[33.75rem] lg:w-[45rem]">
-              <StylizedImage 
+            <div className="w-[33.75rem] flex-none lg:w-[45rem]">
+              {/* <StylizedImage 
                 src={imageLaptop}
                 sizes="(min-width: 1024px) 41rem, 31rem" 
                 className="justify-center lg:justify-end"
-              />
+              /> */}
+                  <svg className="Playground__svg ml-20" viewBox="0 0 100 120">
+                  <defs>
+                    <filter id="filter" x="-20%" y="-20%" width="140%" height="140%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" colorInterpolationFilters="sRGB" filterRes="1">
+                      <feMorphology operator="dilate" radius="10 0" x="0%" y="0%" width="100%" height="100%" in="SourceGraphic" result="morphology1"></feMorphology>
+                    </filter>
+                    <filter id="filter-2" x="-20%" y="-20%" width="140%" height="140%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" colorInterpolationFilters="sRGB" filterRes="1">
+                      <feMorphology operator="dilate" radius="10 2" x="0%" y="0%" width="100%" height="100%" in="SourceGraphic" result="morphology1"></feMorphology>
+                    </filter>
+                    <filter id="filter-3" x="-20%" y="-20%" width="140%" height="140%" filterUnits="objectBoundingBox" primitiveUnits="userSpaceOnUse" colorInterpolationFilters="sRGB" filterRes="1">
+                      <feMorphology operator="dilate" radius="15 0" x="0%" y="0%" width="100%" height="100%" in="SourceGraphic" result="morphology1" filterRes="1"></feMorphology>
+                    </filter>
+                  </defs>
+                  <image x="0%" y="0%" height="120" preserveAspectRatio="xMidYMid slice" xlinkHref="https://images.unsplash.com/photo-1571977144562-3737f035296a?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ" id="my-image"></image>
+              </svg>
             </div>
           </div>
           {/* List item */}
